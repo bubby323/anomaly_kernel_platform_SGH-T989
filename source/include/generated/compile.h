a79c5d1e739b02813f3e249a57a539db04252f1e
Preston Davis <geek.preston@gmail.com>
2011-10-26 16:21:09 -0500

    updated SGH-T989 kernel source to base off of

---
+++ source/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Oct 25 19:15:55 KST 2011"
+#define LINUX_COMPILE_TIME "19:15:55"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "SE-OSS"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
