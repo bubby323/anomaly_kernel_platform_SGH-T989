af8daee4890acb39f35868618cb20f6b8b1a8c23
Shankar <mathkid95@gmail.com>
2011-11-05 19:30:23 -0500

    Add the MinMax governor

--- source/include/linux/cpufreq.h
+++ source/include/linux/cpufreq.h
@@ -375,6 +375,9 @@ extern struct cpufreq_governor cpufreq_gov_lazy;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_MINMAX)
+extern struct cpufreq_governor cpufreq_gov_minmax;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_minmax)
 #endif
 
 
